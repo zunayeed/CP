65c8e87d787c3173af6aaee98ab3ec828b353550
Zunayeed <zunayeed.edu@gmail.com>
2020-10-21 14:09:01 -0400

    Create Sherlock and Valid Strings.java

---
+++ Interview Preparation/Strings/Sherlock and Valid Strings.java
@@ -0,0 +1,33 @@
+    // Complete the isValid function below.
+    static String isValid(String s) {
+            Map<Character, Integer> hm = new HashMap<>();
+    for(int i=0; i<s.length(); i++){
+        if(hm.containsKey(s.charAt(i))){
+            hm.put(s.charAt(i), hm.get(s.charAt(i))+1);
+        }else{
+            hm.put(s.charAt(i), 1);
+        }
+    }
+
+     int min = Collections.min(hm.values());
+     int max = Collections.max(hm.values());
+
+    if(min==max)
+        return "YES";
+     HashMap<Integer,Integer> intMap = new HashMap();
+           for(int count : hm.values())
+           {
+              Integer val = intMap.get(count);
+               if(null != val)
+                   {
+                   intMap.put(count,val+1);
+               }
+               else
+                   {
+                   intMap.put(count,1);
+               }
+           }
+
+    return intMap.size() == 2 && (intMap.get(min) == 1 || intMap.get(max) == 1) && (max-min<=1 || (min==1 && intMap.get(min) == 1))? "YES" : "NO";
+
+    }
